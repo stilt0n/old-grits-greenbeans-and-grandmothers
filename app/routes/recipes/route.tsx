48b4d893f0f8bdf356e627ac83d0596bb469f46e
stilt0n <mattias.leino@arts.cornish.edu>
2024-03-31 14:52:02 -0400

    add recipe card component

--- app/routes/recipes/route.tsx
+++ app/routes/recipes/route.tsx
@@ -1,5 +1,69 @@
+import { useLoaderData } from '@remix-run/react';
+import { Card } from '~/components/card';
+
+export const loader = () => {
+  return {
+    recipes: [
+      {
+        id: 1,
+        name: 'Grits',
+        author: 'grandmother_bot',
+        description: "Grandmother bot's take on a southern classic",
+      },
+      {
+        id: 2,
+        name: 'Grits',
+        author: 'grandmother_bot',
+        description: "Grandmother bot's take on a southern classic",
+      },
+      {
+        id: 3,
+        name: 'Grits',
+        author: 'grandmother_bot',
+        description: "Grandmother bot's take on a southern classic",
+      },
+      {
+        id: 4,
+        name: 'Grits',
+        author: 'grandmother_bot',
+        description: "Grandmother bot's take on a southern classic",
+      },
+      {
+        id: 5,
+        name: 'Grits',
+        author: 'grandmother_bot',
+        description: "Grandmother bot's take on a southern classic",
+      },
+      {
+        id: 6,
+        name: 'Grits',
+        author: 'grandmother_bot',
+        description: "Grandmother bot's take on a southern classic",
+      },
+    ],
+  };
+};
+
 const Recipes = () => {
-  return <h1>Recipes page under construction...</h1>;
+  const { recipes } = useLoaderData<typeof loader>();
+
+  if (!recipes || recipes.length === 0) {
+    <p className='text-center'>There are no recipes to load yet</p>;
+  }
+
+  return (
+    <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-5 justify-items-center'>
+      {recipes?.map((recipe) => (
+        <Card
+          key={recipe.id}
+          title={recipe.name}
+          description={recipe.description}
+          imageUrl='https://upload.wikimedia.org/wikipedia/commons/4/49/Grits_with_cheese%2C_bacon%2C_green_onion_and_poached_egg.jpg'
+          author={recipe.author}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default Recipes;
