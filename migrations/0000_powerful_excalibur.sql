47679cd7de0b5806632510cb21e96ac548ed95b3
stilt0n <mattias.leino@arts.cornish.edu>
2024-04-03 00:05:47 -0400

    migrate tables

--- migrations/0000_powerful_excalibur.sql
+++ migrations/0000_powerful_excalibur.sql
@@ -1,3 +1,9 @@
+CREATE TABLE `authors` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`bio` text
+);
+--> statement-breakpoint
 CREATE TABLE `ingredients` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
@@ -13,17 +19,8 @@ CREATE TABLE `recipes` (
 	`name` text NOT NULL,
 	`description` text NOT NULL,
 	`instructions` text NOT NULL,
-	`author` text,
 	`image_url` text,
-	`user_id` integer NOT NULL,
+	`author_id` integer,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+	FOREIGN KEY (`author_id`) REFERENCES `authors`(`id`) ON UPDATE no action ON DELETE no action
 );
---> statement-breakpoint
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`email` text NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
