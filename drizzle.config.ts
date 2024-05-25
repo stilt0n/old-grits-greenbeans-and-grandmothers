0bcd75c7d333a5791ffe5d842c580a955b9f5a7b
stilt0n <mattias.leino@arts.cornish.edu>
2024-05-25 07:51:29 -0400

    upgrade drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,7 @@ export default {
   schema: './db/schema.ts',
   out: './migrations',
   driver: 'turso',
+  dialect: 'sqlite',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
